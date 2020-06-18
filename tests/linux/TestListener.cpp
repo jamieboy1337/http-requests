b82f7ffc57ede58a242b4762dce67660c3d3311c
jamieboy1337 <jamieboy1337@gmail.com>
2020-06-18 16:21:42 -0700

    testlistener for socket tests

--- tests/linux/TestListener.cpp
+++ tests/linux/TestListener.cpp
@@ -6,6 +6,10 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#include <fcntl.h>
+
+#include <cstdlib>
+
 #include <iostream>
 
 TestListener::TestListener(int buf_size) : buffer_size_(buf_size) {
@@ -19,23 +23,36 @@ void TestListener::Listen(int port) {
   host_fd_ = socket(AF_INET, SOCK_STREAM, 0);
 
   if (host_fd_ == -1) {
-    std::cout << "failed to create socket" << std::endl; 
+    std::cout << "failed to create socket" << std::endl;
+    std::cout << strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  sockaddr_in localhost_sock;
-  inet_pton(AF_INET, "127.0.0.1", &(localhost_sock.sin_addr));
-  localhost_sock.sin_port = htons(port);
+  addrinfo hints;
 
-  success = bind(host_fd_, reinterpret_cast<sockaddr*>(&localhost_sock), sizeof(sockaddr_in));
+  memset((void*)&hints, 0, sizeof(addrinfo));
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  addrinfo* res;
+
+  getaddrinfo(NULL, std::to_string(port).c_str(), &hints, &res);
+
+  sockaddr_in* address = reinterpret_cast<sockaddr_in*>(res->ai_addr);
+  address->sin_port = htons(port);
+
+  success = bind(host_fd_, reinterpret_cast<sockaddr*>(address), sizeof(sockaddr_in));
   if (success != 0) {
     std::cout << "failed to bind!" << std::endl;
+    std::cout << strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
 
   success = listen(host_fd_, SOMAXCONN);
   if (success != 0) {
     std::cout << "failed to listen!" << std::endl;
+    std::cout << strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
 
@@ -46,6 +63,9 @@ void TestListener::Accept() {
 
   sockaddr_storage addr;
   socklen_t addrlen;
+
+  memset(&addr, 0, sizeof(sockaddr_storage));
+  memset(&addrlen, 0, sizeof(socklen_t));
   int accept_fd = accept(host_fd_, reinterpret_cast<sockaddr*>(&addr), &addrlen);
 
   if (accept_fd == -1) {
@@ -56,7 +76,15 @@ void TestListener::Accept() {
 
   // socket is closed by HTTPSocket dtor
   HTTPSocket accepted_socket(accept_fd);
-  accepted_socket.Read(buffer_, buffer_size_);
+
+
+  char* bufptr = buffer_;
+  for (;;) {
+    accepted_socket.Read(bufptr, 1);
+    if (*bufptr++ == '\n') {
+      break;
+    }
+  }
 }
 
 const char* TestListener::GetMessage() {
