84d4794339a268022ad5b10db5b68d7436873634
jamieboy1337 <jamieboy1337@gmail.com>
2020-06-18 17:44:23 -0700

    starting over on response object

--- src/HTTPResponse.cpp
+++ src/HTTPResponse.cpp
@@ -1,13 +1,5 @@
 #include "HTTPResponse.hpp"
 
-HTTPResponse::HTTPResponse(std::string response) {
+HTTPResponse::HTTPResponse(std::string header) {
   // TODO: parse the response once we have it ;p
-}
-
-std::string HTTPResponse::GetHeader(const std::string& key) {
-  return headers_[key];
-}
-
-const std::string& HTTPResponse::GetBody() {
-  return body_; 
 }
